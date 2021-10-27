b71c646241777914694582b9dbe1704a4b89011c
Andy Incorporated <59496249+AndrewR0@users.noreply.github.com>
2021-10-26 21:30:36 -0400

    Time: 128 ms (51.22%), Space: 14.6 MB (38.90%) - LeetHub

---
+++ build-array-from-permutation/build-array-from-permutation.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        
+        ans = [nums[nums[i]] for i in range(len(nums))]
+        
+        return ans
+        
+        
