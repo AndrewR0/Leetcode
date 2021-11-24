543032fc22fe65252cb4560f1546551572085c1c
Andy Incorporated <59496249+AndrewR0@users.noreply.github.com>
2021-11-23 21:50:18 -0500

    Time: 32 ms (66.18%), Space: 14.1 MB (89.32%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        count = 0
+        for i in format(n, "b"):
+            if i == "1":
+                count += 1
+        return count
