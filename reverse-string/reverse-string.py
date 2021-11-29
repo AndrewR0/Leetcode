85192b953876c8026c42f20e8d720f3f8a2313da
Andy Incorporated <59496249+AndrewR0@users.noreply.github.com>
2021-11-28 21:35:03 -0500

    Time: 208 ms (43.41%), Space: 18.7 MB (44.18%) - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -3,7 +3,16 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        x = s[::-1]
+        
+        left, right = 0, len(s)-1
+        
+        while right > left:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+        
+        '''x = s[::-1]
         
         for i in range(len(x)):
             s[i] = x[i]
+        '''
