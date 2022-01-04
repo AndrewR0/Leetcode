41a1f1dcb2cf8e9c3e0f6744d82c86e5a3db59d9
Andy Incorporated <59496249+AndrewR0@users.noreply.github.com>
2022-01-04 14:51:40 -0500

    Time: 1504 ms (7.73%), Space: 19 MB (63.30%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        
+        if len(trust) == 0 and n == 1:
+            return 1
+        
+        foundNodes = []
+        for i in range(len(trust)):
+            if trust[i][0] not in foundNodes:
+                foundNodes.append(trust[i][0])
+            if trust[i][1] not in foundNodes:
+                foundNodes.append(trust[i][1])
+        
+        nodes = {j:0 for j in foundNodes}
+        found = {}
+        for x in range(len(trust)):
+            nodes[trust[x][0]] += 1
+            if trust[x][1] in found:
+                found[trust[x][1]] += 1
+            else:
+                found[trust[x][1]] = 1
+        
+        #print(nodes, found, foundNodes)
+        
+        for y in nodes.keys():
+            if nodes[y] == 0 and found[y] == len(foundNodes)-1:
+                return y
+            
+        return -1
+    
