79ee6bd3af78a225c042630e7a780a8e9d5b6c7b
Andy Incorporated <59496249+AndrewR0@users.noreply.github.com>
2022-01-11 11:32:56 -0500

    Time: 762 ms (6.79%), Space: 25.7 MB (52.08%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        found = {}
+        
+        for i in nums:
+            if i in found:
+                return True
+            else:
+                found[i] = 1
+        return False
