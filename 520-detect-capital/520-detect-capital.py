a8e63c5c772cceae708a5dc42aa94aabbbdd4d7d
Andy Incorporated <59496249+AndrewR0@users.noreply.github.com>
2022-01-23 21:12:01 -0500

    Time: 40 ms (42.29%), Space: 14.2 MB (46.33%) - LeetHub

--- 520-detect-capital/520-detect-capital.py
+++ 520-detect-capital/520-detect-capital.py
@@ -1,3 +1,4 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        return re.fullmatch(r"[A-Z]*|.[a-z]*", word)
+        #return re.fullmatch(r"[A-Z]*|.[a-z]*", word)
+        return word.isupper() or word.islower() or word.istitle()
